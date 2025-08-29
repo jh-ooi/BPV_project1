1a9c94d1cad3f2a4c1522c7352c3609903421884
jh-ooi <90020568+jh-ooi@users.noreply.github.com>
2025-08-29 12:44:57 +0800

    Prepared the mapping table for each target antihypertensive drugs, and identified their drug class and regimen.
    Save cache file.
    Further improved the algorithm in identifying potential antihypertensive drugs.
    Mapped the drugs into their respective drug classes and types of regimen based on their ingredients.

---
+++ antiHTN_drug_filtering_mapping.py
@@ -0,0 +1,229 @@
+## after extracting all the potential antihypertensive drugs (generic/brand name/ingredient)
+## make sure all are antihypertensive drugs and map the drug names into respective drug class and regimen type
+
+
+import pandas as pd
+import requests
+import requests_cache
+requests_cache.install_cache(cache_name='http_cache',backend='filesystem')
+from typing import Set, Tuple
+
+# == Ingredient --> Class mapping ==
+def get_atc_id(rxcui_id: str):
+
+    atc_id = set()
+    ing_name_set = set()
+    url = f"https://rxnav.nlm.nih.gov/REST/rxclass/class/byRxcui.json"
+    params = {"rxcui": rxcui_id, "relaSource": "ATC"}
+    r = requests.get(url, params=params)
+    data = r.json()
+
+    for classDrug in data.get('rxclassDrugInfoList', {}).get('rxclassDrugInfo', []):
+        ingredient_type = classDrug.get("minConcept").get("tty")
+        # print(ingredient_type)
+        if ingredient_type == "IN":
+            minConceptItem = classDrug.get("rxclassMinConceptItem", {})
+            # print(minConcept)
+            ATC_classID = minConceptItem.get("classId")
+            # if is_target_antihypertensive((ATC_classID)):
+            ingredientName = classDrug.get("minConcept").get("name")
+
+            if ATC_classID:
+                atc_id.add((ingredientName, ATC_classID))
+            # print(ATC_classID)
+            # ingredientID = classDrug.get("minConcept").get("rxcui")
+
+            # print(ingredientName)
+            # print(ATC_classID)
+
+            # if ATC_classID and is_target_antihypertensive(ATC_classID):
+                # ing_name_set.add(ingredientName)
+                # atc_id.add((ingredientName, ATC_classID))
+                # print(f"name: {name}")
+                # print(f"classID: {classID}")
+
+    # fallback: if no IN entries, try PIN (precise ingredient)
+    if not atc_id:
+        # print("hh")
+        for classDrug in data.get('rxclassDrugInfoList', {}).get('rxclassDrugInfo', []):
+            ingredient_type = classDrug.get("minConcept").get("tty")
+            # print(ingredient_type)
+            if ingredient_type == "PIN":
+                minConceptItem = classDrug.get("rxclassMinConceptItem", {})
+                ATC_classID = minConceptItem.get("classId")
+                # if is_target_antihypertensive((ATC_classID)):
+                ingredientName = classDrug.get("minConcept").get("name")
+                if ATC_classID:
+                    atc_id.add((ingredientName, ATC_classID))
+
+    # print(atc_id)
+    # return ing_name_set, atc_id
+    return atc_id if atc_id else ()
+
+def get_atc_for_brand_rxcui(brand_rxcui: str):
+    ing_list = []
+    final_ing = set()
+    all_classId = set()
+
+    url = f"https://rxnav.nlm.nih.gov/REST/rxcui/{brand_rxcui}/historystatus.json"
+    rel_in = requests.get(url).json()
+
+    dc = rel_in.get('rxcuiStatusHistory', {}).get('derivedConcepts', {}) or {}
+    ings = dc.get('ingredientConcept') or []
+
+    for i in ings:
+        rx = i.get('ingredientRxcui') or ""
+        ingredient_name = i.get('ingredientName') or ""
+        ing_list.append((rx, ingredient_name))
+        # final_ing.add(ingredient_name)
+
+    for ing_ID, ing_Name in ing_list:
+        ingName_ATC_code = get_atc_id(ing_ID)
+        # ingName_ATC_code = filter_atc(ing_Name, classID)
+        # print(classID)
+        # print(type(classID))
+        # final_ing.update(ingredient_NAMESET)
+        all_classId.update(ingName_ATC_code)
+
+
+    return all_classId if all_classId else ()
+
+def keep_brand(pairs):
+    ingredients = {name for name, _ in pairs}
+    for ing in ingredients:
+        atcs_for_ing = [a for n, a in pairs if n == ing]
+        if not any(is_target_antihypertensive(a) for a in atcs_for_ing):
+            return False
+    return True
+
+def is_target_antihypertensive(atc_id) -> bool:
+    if atc_id.startswith(("C02A", "C02B", "C02C", "C02D", "C02K", "C03A", "C03B",
+                          "C03C", "C03D", "C03E","C07A", "C08C", "C08D", "C08E",
+                          "C09A", "C09C", "C09X")):
+        return True
+    if atc_id.startswith("C02KX"):
+        return False
+    if atc_id.startswith("C09XX"):
+        return False
+
+    return False
+
+def regimen_type(ingredients: set[str]) -> str:
+    # Count unique ingredients
+    count = len(set(ingredients))
+    if count == 1:
+        return "monotherapy"
+    elif count == 2:
+        return "combination of 2 drugs"
+    elif count == 3:
+        return "combination of 3 drugs"
+    elif count == 4:
+        return "combination of 4 drugs"
+    else:
+        return "combination of > 5 drugs"
+
+# map to drug class - from ATC code to drug Class
+ATC_to_class_dict = {"C09A": "ACEi", "C09C": "ARB", "C09X": "Other RAS",
+                "C08C": "CCB", "C08D": "CCB", "C08E": "CCB",
+                "C07A": "Beta-Blocker",
+                "C03A": "Diuretics: thiazide", "C03B": "Diuretics: non-thiazide", "C03C": "Diuretics: loop diuretics",
+                "C03D": "Diuretics: potassium-sparing agent", "C03E": "Diuretics: potassium-sparing agent combination",
+                "C02A": "Other: centrally acting", "C02B": "Other: ganglion blocking", "C02C": "Other: peripherally acting (alpha)",
+                "C02D": "Other: vasodilator", "C02K": "Other"
+                }
+
+def class_from_ATC(ATC: set)->set:
+    results = set()
+    for code in ATC:
+        drugClass = ATC_to_class_dict.get(code[:4], "Not target antihypertensives")
+        # print(drugClass)
+        # print(type(drugClass))
+        results.add(drugClass)
+        # print (results)
+    return results
+
+
+def main():
+    # === load the RxNorm potential antiHTN drug list ===
+    df = pd.read_csv("antiHTN_drug_allBRANDs_ingredients_list2.csv", low_memory=False)
+    # OBJECT_NAME = df['name']
+    # OBJECT_RXCUI = df['rxcui']
+    # OBJECT_STATUS = df['status']
+
+    kept_rows = []
+
+    for _, row in df.iterrows():
+        obj_rxcui = row["rxcui"]
+        obj_status = row["status"]
+        # print(f"{obj_rxcui}: {obj_status}")
+
+        if obj_status in ("active", "obsolete"):
+            ING_NAME_ATC_PAIRS = get_atc_for_brand_rxcui(obj_rxcui)
+
+        elif obj_status == "ingredient":
+            ING_NAME_ATC_PAIRS = get_atc_id(obj_rxcui)
+
+        # print(ING_NAME_ATC_PAIRS)
+        # print(obj_rxcui)
+
+        if ING_NAME_ATC_PAIRS:
+            if keep_brand(ING_NAME_ATC_PAIRS):
+
+                ING_NAME = {name for name, _ in ING_NAME_ATC_PAIRS}
+                ING_ATC_ID = {atc for _, atc in ING_NAME_ATC_PAIRS if is_target_antihypertensive(atc)}
+
+                regimen = regimen_type(ING_NAME)
+                drug_class = class_from_ATC(ING_ATC_ID)
+
+                # if any(is_target_antihypertensive(ing_atc_id) for ing_atc_id in ING_ATC_ID):
+                kept_rows.append({
+                    "name": row["name"],
+                    "rxcui": row["rxcui"],
+                    "status": row["status"],
+                    "ingredient": ", ".join(sorted(ING_NAME)) if ING_NAME else "",
+                    "ATC_code": ", ".join(sorted(ING_ATC_ID)) if ING_ATC_ID else "",
+                    "drugClass": ", ".join(sorted(drug_class)) if drug_class else "",
+                    "regimen": regimen
+                })
+
+    df_kept = pd.DataFrame(kept_rows)
+    df_kept.to_csv("antiHTN_drug_filtered_MAPPING2.csv", index=False)
+
+if __name__ == "__main__":
+    main()
+    # ING_NAME_ATC_PAIRS = get_atc_for_brand_rxcui(140587)
+    # print(ING_NAME_ATC_PAIRS)
+    # ING_NAME_ATC_PAIRS = get_atc_for_brand_rxcui(303838)
+    # print(ING_NAME_ATC_PAIRS)
+    # ING_NAME, ING_ATC_ID = set(zip(*ING_NAME_ATC_PAIRS))
+    # print(ING_NAME)
+    # print(ING_ATC_ID)
+    # print('------')
+    # ING_NAME_ATC_PAIRS = get_atc_id(140587)
+    # print(ING_NAME_ATC_PAIRS)
+    # if ING_NAME_ATC_PAIRS:
+    #     ING_NAME = {name for name, _ in ING_NAME_ATC_PAIRS}
+    #     ING_ATC_ID = {atc for _, atc in ING_NAME_ATC_PAIRS}
+    # print('final')
+    # print(ING_NAME)
+    # print(ING_ATC_ID)
+
+# in the list,
+# drop_list = {
+#     "bepridil", "bethanidine", "betanidine", "canrenoate potassium", "potassium canrenoate",
+#     "canrenone", "deserpidine", "rescinnamine", "reserpine", "methoserpidine", "rauzide",
+#     "serpasil", "serpazide", "hydroserpine", "diazoxide", "proglycem", "ethacrynic acid",
+#     "fendiline", "lidoflazine", "gallopamil", "perhexiline", "mibefradil", "finerenone",
+#     "mersalyl", "metyrosine", "metirosine", "nitroprusside", "pargyline", "sotalol",
+#     "theobromine", "trimethaphan", "trimetaphan",
+#     "timolol", "timoptic", "timoptol", "istalol", "betimol", "betoptic s", "ocupress",
+#     "semintra", "cardalis", "entresto", "sparsentan", "filspari",
+#     "hemangeol", "rapiblyk", "onyda", "vykat", "froop"
+# }
+#
+# df3 = pd.read_csv("antiHTN_drug_filtered_MAPPING3.csv")
+# # Filter out non-antihypertensives
+# df_filtered = df3[~df3["name"].str.lower().isin(drop_list)]
+#
+# # Save or overwrite the cleaned table
+# df_filtered.to_csv("antiHTN_drug_filtered_CLEAN.csv", index=False)
---
+++ identify_antiHTN_drug_ingredients_brand.py
@@ -0,0 +1,252 @@
+import pandas as pd
+import requests
+import requests_cache
+# from concurrent.futures import ThreadPoolExecutor, as_completed
+requests_cache.install_cache(cache_name='http_cache',backend='filesystem')
+
+# === Identify antiHTN drug members using RxCUI based on ATC ===
+def get_drug_members_by_class(atc_class_id):
+    url = "https://rxnav.nlm.nih.gov/REST/rxclass/classMembers.json"
+    params = {"classId": atc_class_id, "relaSource": "ATC"}
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    # print(data)
+
+    members = data['drugMemberGroup']['drugMember']
+    # members = data.get('drugMemberGroup', {}).get('drugMember', {})
+    ingredientList = []
+
+    for member in members:
+        min_concept = member.get("minConcept", {})
+        name = min_concept.get("name")
+        ingredientList.append(name)
+
+    # print(ingredientList)
+    return ingredientList
+
+def get_rxcui(name):
+    url = "https://rxnav.nlm.nih.gov/REST/rxcui.json"
+    params = {"name": name, "search": 2}
+    r = requests.get(url, params=params)
+    data = r.json()
+    # return data['idGroup']['rxnormID']
+    return data.get("idGroup", {}).get("rxnormId", [None])[0]
+
+def get_synonyms(rxcui):
+    url = f"https://rxnav.nlm.nih.gov/REST/rxcui/{rxcui}/allProperties.json"
+    params = {"prop": "names"}
+    r = requests.get(url, params=params)
+    data = r.json()
+    # print('synonyms: ')
+    # print(data)
+    synonyms = set()
+    for prop in data["propConceptGroup"]["propConcept"]:
+    # for prop in data.get("propConceptGroup", {}).get("propConcept", []):
+        synonym = prop["propValue"]
+        # print(type(synonym))
+        synonyms.add(synonym.lower())
+
+    # print("synonyms", synonyms)
+    return synonyms
+
+
+def passes_atc_gate(rxcui):
+    url = f"https://rxnav.nlm.nih.gov/REST/rxclass/class/byRxcui.json"
+    params = {"rxcui": rxcui, "relaSource": "ATC"}
+    r = requests.get(url, params=params)
+    data = r.json()
+
+    classes = []
+    for item in data.get("rxclassDrugInfoList", {}).get("rxclassDrugInfo", []) or []:
+        cid = item.get("rxclassMinConceptItem", {}).get("classId")
+        if cid:
+            classes.append(cid)
+    # print('atc code')
+    # print(classes)
+    # allow only antihypertensives (C02, C03, C07, C08, C09)
+    # exclude C09D combos and dermatology (D..)
+    if any(cid.startswith("C02KX") for cid in classes):  # exclude pulmonary hypertension
+        return False
+    if any(cid.startswith("C09XX") for cid in classes):
+        return False
+    if any(cid.startswith("C09DX") for cid in classes):
+        return False
+    if any(cid[0] == "D" for cid in classes):  # exclude dermatology e.g. hair minoxidil
+        return False
+
+    # if any(cid[:4] in {"C02A","C02B","C02C","C02D","C02K","C03A","C03B","C03C","C03D","C03E",
+    #                    "C07A","C08C","C08D","C08E","C09A","C09C","C09X"} for cid in classes):
+    if any(cid[:3] in {"C02", "C03", "C07", "C08", "C09"} for cid in classes):
+        return True
+
+    return False
+
+def build_all_ingredient_list(ingredient_list):
+    # all_names = set()
+    name_to_rxcui_dict = {}
+    for name in ingredient_list:
+        # print('ingredient name')
+        # print(name)
+        rxcui = get_rxcui((name))
+        if not rxcui:
+            print(f" No RxCUI found for {name}")
+            continue
+
+        if passes_atc_gate(rxcui):
+            name_to_rxcui_dict[name] = rxcui
+
+            synonyms = get_synonyms(rxcui)
+            # all_names.update(synonyms)
+            for syn in synonyms:
+                syn_rxcui = get_rxcui(syn)
+                if syn_rxcui and passes_atc_gate(syn_rxcui):
+                    # all_names.add(syn.lower())
+                    name_to_rxcui_dict[syn] = syn_rxcui
+
+    return name_to_rxcui_dict
+
+DISALLOWED_FORMS = (
+    "ophthalmic", "eye", "otic", "topical", "patch", "transdermal",
+    "injection", "intravenous", "iv", "intramuscular", "subcutaneous",
+    "inhalation", "nasal", "rectal", "vaginal", "buccal", "sublingual",
+    "irrigation", "spray", "drops", "solution", "suspension", "elixir",
+    "gel", "lotion", "cream", "ointment", "foam", "paste"
+)
+
+# find obsolete brands from the ingredient list
+def fetch_all_brand(status:str):
+
+    url1 = "https://rxnav.nlm.nih.gov/REST/allstatus.json"
+    params1 = {"status": status}
+    r1 = requests.get(url1, params=params1)
+    data1 = r1.json()
+    # print(data1)
+    out = []
+    out1 = []
+
+    for d1 in data1['minConceptGroup']['minConcept']:
+
+        if d1['tty'] == 'BN':
+            out.append((d1.get("name"), d1.get('rxcui')))
+            # i = i + 1
+        if d1['tty'] == 'SBD':
+            out1.append((d1.get("name"), d1.get('rxcui')))  ## list with SBD [ingredient + dosage form + [brandname]
+    kept_brand = []
+
+    for brand, brand_rx in out:
+
+        brand_matched_sbd = [sbd_name for sbd_name, _ in out1 if brand in sbd_name]
+        # print(brand_matched_sbd)
+        if not brand_matched_sbd:
+            # dropped_brand.append((brand, brand_rx))
+            continue
+
+        kept = True
+        for sbd in brand_matched_sbd:
+            sbd_low = sbd.lower()
+            if any(form in sbd_low for form in DISALLOWED_FORMS):
+                kept = False
+                break
+        if kept:
+            kept_brand.append((brand, brand_rx))
+
+    return kept_brand #a list of (bn_rxcui, brand_name) pairs.
+
+# for each brand, call historystatus to look for their main ingredient
+def bn_ingredients(bn_rxcui: str)->list:
+    url1 = f"https://rxnav.nlm.nih.gov/REST/rxcui/{bn_rxcui}/historystatus.json"
+    rel_in = requests.get(url1).json()
+    # print(rel_in)
+    ing_list = []
+
+    dc = rel_in.get('rxcuiStatusHistory', {}).get('derivedConcepts', {}) or {}
+    ings = dc.get('ingredientConcept') or []
+    for i in ings:
+        rx = i.get('ingredientRxcui') or ""
+        ingredient_name = i.get('ingredientName') or ""
+
+        ing_list.append((rx, ingredient_name))
+
+    # print(ings)
+    return ing_list # a list of ingredients (in tuples) for that particular brand
+
+def find_brands_for_ingredients(all_ingredient_dic: dict, status:str)->dict:
+    bn_list = fetch_all_brand(status) # [(bn_rxcui, bn_name), ...] in a list of tuples
+    brands_dict = {}
+    all_ingredient_rxcui_list = list(all_ingredient_dic.values())
+    all_ingredient_rxcui_list = {str(x) for x in all_ingredient_rxcui_list}
+
+    for bn_name, bn_rx in bn_list:
+        hit_found = False
+        ingredient_list_in_brand = bn_ingredients(bn_rx)
+        all_ingredient_ID = [ing_id for ing_id, ing_name in ingredient_list_in_brand]
+        # print(all_ingredient_ID)
+        all_ingredient_name = [ing_name for ing_id, ing_name in ingredient_list_in_brand]
+
+        hit_found = any(ing_ids in all_ingredient_rxcui_list for ing_ids in all_ingredient_ID)
+        if hit_found:
+            brands_dict[bn_name] = {
+                "name": bn_name,
+                "rxcui": bn_rx,
+                "minIngredients_ID": all_ingredient_ID,
+                "minIngredients_name": all_ingredient_name,
+                "status": status
+            }
+
+    return brands_dict
+
+
+def main():
+    ## STEP 1: Identify all ingredients for antihypertensive medications
+    atc_classes = ['C02', 'C03', 'C07', 'C08', 'C09']  # ATC codes for antihypertensives
+    all_names = set()
+
+    for class_id in atc_classes:
+        print(f"Fetching drugs from ATC: {class_id}")
+        class_names = get_drug_members_by_class(class_id)
+        all_names.update(class_names)
+    # print(all_names) # set of all antihypertensive ingredients
+    ingredient_set = set()
+
+    for name in all_names:
+        name = name.replace("/", ",")
+        ingredients = [part.strip() for part in name.split(",") if part.strip()]
+        ingredient_set.update(ingredients)
+    ingredients_sorted = sorted(ingredient_set) # set of all normalized antihypertensive ingredients
+    # print(ingredients_sorted)
+
+    # Step 2: After identifying ingredients, looking through RxNORM database to find their ID and other potential ingredient name
+    all_ingredient_list_dict = build_all_ingredient_list(ingredients_sorted)
+    # print(all_ingredient_list)
+
+    # Step 3: find active brand name
+    active_brand_dict = find_brands_for_ingredients(all_ingredient_list_dict, status="active")
+
+    # Step 4: find obsolete brand from ingredient
+    obsolete_brand_dict = find_brands_for_ingredients(all_ingredient_list_dict, status="obsolete")
+
+    # Step 5: combine both active and obsolete brand dictionaries
+    # Create DataFrames separately
+    df = pd.DataFrame.from_dict(all_ingredient_list_dict, orient='index')
+    df.columns = ['rxcui']
+    df = df.reset_index().rename(columns={"index": "name"})
+    df["status"] = "ingredient"
+    df = df[['name', 'rxcui', 'status']]
+
+    df1 = pd.DataFrame.from_dict(active_brand_dict, orient="index")
+    df1 = df1[["name", "rxcui", "status"]]
+    df2 = pd.DataFrame.from_dict(obsolete_brand_dict, orient="index")
+    df2 = df2[["name", "rxcui", "status"]]
+
+    # Combine
+    df_comb = pd.concat([df, df1, df2])
+
+    pd.DataFrame(df_comb).to_csv("antiHTN_drug_allBRANDs_ingredients_list.csv",index=False)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
